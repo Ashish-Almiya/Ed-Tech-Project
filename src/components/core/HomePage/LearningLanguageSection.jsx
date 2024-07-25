fe0c611d400c0a53b20585aaff42be0c5fd70053
Lb181808 <lb181808@gmail.com>
2024-07-25 19:24:27 +0530

    created half part of Learnign Language Section

--- src/components/core/HomePage/LearningLanguageSection.jsx
+++ src/components/core/HomePage/LearningLanguageSection.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
+import HighlightText from './HighlightText'
 
 const LearningLanguageSection = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div>
+            <div className="text-4xl font-semibold text-center my-10">
+                Your swiss knife for
+                <HighlightText text={"learning any language"} />
+                <div className="text-center text-richblack-700 font-medium lg:w-[75%] mx-auto leading-6 text-base mt-3">
+                  Using spin making learning multiple languages easy. with 20+
+                  languages realistic voice-over, progress tracking, custom schedule
+                  and more.
+                </div>
+                
+              </div>
+    
+              
+        </div>
+      )
 }
 
 export default LearningLanguageSection
