ddf9a3e2f91476b656c39c0de985904b4b9f763a
Lb181808 <lb181808@gmail.com>
2024-07-25 21:25:39 +0530

    Completed Learning Language Section

--- src/components/core/HomePage/Button.js
+++ src/components/core/HomePage/Button.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const CTAButton = ({children,active,linkto}) => {
   return (
     <Link to={linkto}>
-        <div className={`z-10 text-center text-[17px] px-6 py-3 rounded-md font-bold ${active?"bg-yellow-50 text-black":"bg-richblack-800 text-white"} hover:scale-95 transition-all duration-200`}>
+        <div className={`z-10 text-center text-[17px] px-6 py-2 rounded-md font-bold ${active?"bg-yellow-50 text-black":"bg-richblack-800 text-white"} hover:scale-95 transition-all duration-200`}>
             {children}
         </div>
     </Link>
