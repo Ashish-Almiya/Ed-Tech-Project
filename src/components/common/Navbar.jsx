5f4772cc24f7dfdc46cbb914e46e2bd998be7272
Lb181808 <lb181808@gmail.com>
2024-07-29 01:17:05 +0530

    created reducer function for auth, cart and profile section and also created some part of navbar

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -3,11 +3,19 @@ import { Link } from 'react-router-dom'
 import logo from "../../assets/Logo/Logo-Full-Light.png"
 import {NavbarLinks} from "../../data/navbar-links.js"
 import { matchPath,useLocation } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { CiShoppingCart } from "react-icons/ci";
 
 
 
 
 const Navbar = () => {
+
+    //fetching reducer
+    const {token}=useSelector((state)=>state.auth)
+    const {user}=useSelector((state)=>state.profile)
+    const {totalItems}=useSelector((state)=>state.cart)
+
     const location=useLocation()
 
     const matchRoute=(route)=>{
@@ -43,7 +51,39 @@ const Navbar = () => {
         </nav>
 
         {/* Login/SignUp/Dashboard */}
-        <div className='fles gap-x-4 items-center'></div>
+        <div className='navListLeft items-center gap-x-4 md:flex'>
+            {
+                user && user?.accountType!="Instructor" &&(
+                    <Link to={"/dashboard/cart"} className='relative'>
+                    <CiShoppingCart className='text-2xl text-richblack-100' />
+                    {
+                        totalItems>0 && (
+                            <span className="absolute -bottom-2 -right-2 grid h-5 w-5 place-items-center overflow-hidden rounded-full bg-richblack-600 text-center text-xs font-bold text-yellow-100">
+                                {totalItems}
+                            </span>
+                        )
+                    }
+                    </Link>
+                )
+            }
+            {
+                token===null && (
+                    <Link to="/login">
+                        <button className="">Log in</button>
+                    </Link>
+                )
+            }
+            {
+                token===null && (
+                    <Link to="/signup">
+                        <button className="">Sign Up</button>
+                    </Link>
+                )
+            }
+            {
+                
+            }
+        </div>
       </div>
     </div>
   )
