433d31cb1320f23e3089f84b78daa63b172520eb
Lb181808 <lb181808@gmail.com>
2024-07-28 23:23:47 +0530

    Competed some portion of navbar

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -14,39 +14,39 @@ const Navbar = () => {
         return matchPath({path:route},location.pathname)
     }
 
-//   return (
-//     <div className="flex h-14 items-center justify-center border-b-[1px] border-b-richblack-700">
-//       <div className="flex w-11/12 max-w-maxContent items-center justify-between">
-//       {/* added images */}
-//         <Link to="/">
-//             <img className="" width={160} height={32} src={logo} loading='lazy' />
-//         </Link>
-//         {/* NAV LINK */}
-//         <nav className="">
-//             <ul className="flex gap-6 text-richblack-25">
-//             {
-//                 NavbarLinks.map((el,index)=>(
-//                     <li className="" key={index}>
-//                         {
-//                             el.title==="Catalog"?(<div></div>):(
-//                                 <Link to={el?.path}>
-//                                     <p className={`${matchRoute(el?.path)?"text-yellow-25":"text-richblack-25"}`}>
-//                                         {el.title}
-//                                     </p>
-//                                 </Link>
-//                             )
-//                         }
-//                     </li>
-//                 ))
-//             }
-//             </ul>
-//         </nav>
+  return (
+    <div className="flex h-14 items-center justify-center border-b-[1px] border-b-richblack-700">
+      <div className="flex w-11/12 max-w-maxContent items-center justify-between">
+      {/* added images */}
+        <Link to="/">
+            <img className="" width={160} height={32} src={logo} loading='lazy' />
+        </Link>
+        {/* NAV LINK */}
+        <nav className="">
+            <ul className="flex gap-6 text-richblack-25">
+            {
+                NavbarLinks.map((el,index)=>(
+                    <li className="" key={index}>
+                        {
+                            el.title==="Catalog"?(<div></div>):(
+                                <Link to={el?.path}>
+                                    <p className={`${matchRoute(el?.path)?"text-yellow-25":"text-richblack-25"}`}>
+                                        {el.title}
+                                    </p>
+                                </Link>
+                            )
+                        }
+                    </li>
+                ))
+            }
+            </ul>
+        </nav>
 
-//         {/* Login/SignUp/Dashboard */}
-//         <div className='fles gap-x-4 items-center'></div>
-//       </div>
-//     </div>
-//   )
+        {/* Login/SignUp/Dashboard */}
+        <div className='fles gap-x-4 items-center'></div>
+      </div>
+    </div>
+  )
 }
 
 export default Navbar
