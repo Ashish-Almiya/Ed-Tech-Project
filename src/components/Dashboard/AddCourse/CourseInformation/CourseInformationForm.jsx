a040efffb127462a814ce80a99624e4c041a1bac
Lb181808 <lb181808@gmail.com>
2024-08-09 23:12:55 +0530

    Started Crating file upload section

--- src/components/Dashboard/AddCourse/CourseInformation/CourseInformationForm.jsx
+++ src/components/Dashboard/AddCourse/CourseInformation/CourseInformationForm.jsx
@@ -118,7 +118,7 @@ const CourseInformationForm = () => {
                 setValue={setValue}
                 getValues={getValues}
             />
-            <Upload/>
+           
     </form>
   )
 }
